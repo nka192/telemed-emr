8300a13a0e3e17667866112e8d1cb0ea9186bbc2
Nayoung Kim <nka50@sfu.ca>
2025-11-11 03:27:21 -0800

    Initial commit - setup Spring Boot + Angular telemedicine project

---
+++ src/main/java/com/nayoung/telemed/TelemedEmrApplication.java
@@ -0,0 +1,13 @@
+package com.nayoung.telemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedEmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedEmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayoung/telemed/TelemedEmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayoung.telemed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedEmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
