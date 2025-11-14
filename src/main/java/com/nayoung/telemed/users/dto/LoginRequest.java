9fd643a1ad88728a96d6193577f27e00998f5896
Nayoung Kim <nka50@sfu.ca>
2025-11-14 15:10:46 -0800

    feat: add LoginRequest DTO with validation annotations

---
+++ src/main/java/com/nayoung/telemed/users/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.nayoung.telemed.users.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
