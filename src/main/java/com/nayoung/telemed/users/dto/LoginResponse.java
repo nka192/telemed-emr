cf36f47007d40c1e057738b8cb0b13b402bd7ea2
Nayoung Kim <nka50@sfu.ca>
2025-11-14 15:48:53 -0800

    feat: add LoginResponse DTO with token and roles

---
+++ src/main/java/com/nayoung/telemed/users/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.nayoung.telemed.users.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class LoginResponse {
+
+    private String token;
+
+    private List<String> roles;
+}
