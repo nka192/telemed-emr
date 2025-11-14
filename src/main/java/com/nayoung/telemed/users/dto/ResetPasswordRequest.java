7a4cc19d30d663b0e2cc11f8559ca398b41f1db1
Nayoung Kim <nka50@sfu.ca>
2025-11-14 14:51:16 -0800

    feat: add ResetPasswordRequest DTO for password reset flow

---
+++ src/main/java/com/nayoung/telemed/users/dto/ResetPasswordRequest.java
@@ -0,0 +1,23 @@
+package com.nayoung.telemed.users.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResetPasswordRequest {
+    // wil be used to request password reset
+    private String email;
+
+    // will be used to set new password
+    private String code;
+    private String newPassword;
+}
