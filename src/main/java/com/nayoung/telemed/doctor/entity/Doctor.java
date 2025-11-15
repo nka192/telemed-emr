d14c9575dfb271d995a7c484964c91b7582d508e
Nayoung Kim <nka50@sfu.ca>
2025-11-15 00:54:56 -0800

    feat: add Doctor entity with specialization, license, user, and appointments

---
+++ src/main/java/com/nayoung/telemed/doctor/entity/Doctor.java
@@ -0,0 +1,43 @@
+package com.nayoung.telemed.doctor.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nayoung.telemed.appointment.entity.Appointment;
+import com.nayoung.telemed.enums.Specialization;
+import com.nayoung.telemed.users.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "doctors")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Specialization specialization;
+
+    private String licenseNumber;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Appointment> appointments;
+}
