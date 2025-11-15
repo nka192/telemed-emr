5e962b3332790a80c111b7946963b62315c28d87
Nayoung Kim <nka50@sfu.ca>
2025-11-15 00:14:06 -0800

    feat: add Notification entity

---
+++ src/main/java/com/nayoung/telemed/notification/entity/Notification.java
@@ -0,0 +1,39 @@
+package com.nayoung.telemed.notification.entity;
+
+import com.nayoung.telemed.enums.NotificationType;
+import com.nayoung.telemed.users.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+
+    private String recipient;
+
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type; // EMAIL / SMS / PUSH
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
