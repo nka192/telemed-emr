55763e50645985eb2971fd94536e7af1044aaae9
Nayoung Kim <nka50@sfu.ca>
2025-11-15 00:47:19 -0800

    feat: add PatientRepo with findByUser method

--- src/main/java/com/nayoung/telemed/patient/repo/PatientRepo.java
+++ src/main/java/com/nayoung/telemed/patient/repo/PatientRepo.java
@@ -1,6 +1,12 @@
 package com.nayoung.telemed.patient.repo;
 
+import com.nayoung.telemed.patient.entity.Patient;
+import com.nayoung.telemed.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepo extends JpaRepository {
+import java.util.Optional;
+
+public interface PatientRepo extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByUser(User user);
 }
