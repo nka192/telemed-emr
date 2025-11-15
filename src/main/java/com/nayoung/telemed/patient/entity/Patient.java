3edd6a3e80d6c85d860904eaa32311259fd688e5
Nayoung Kim <nka50@sfu.ca>
2025-11-15 00:43:13 -0800

    feat: add Patient entity

---
+++ src/main/java/com/nayoung/telemed/patient/entity/Patient.java
@@ -0,0 +1,53 @@
+package com.nayoung.telemed.patient.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nayoung.telemed.appointment.entity.Appointment;
+import com.nayoung.telemed.enums.BloodGroup;
+import com.nayoung.telemed.enums.Genotype;
+import com.nayoung.telemed.users.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "patients")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    private String phone;
+
+    @Lob // stores allergies as a comma-separated string
+    private String knownAllergies;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroup bloodGroup;
+
+    @Enumerated(EnumType.STRING)
+    private Genotype genotype;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Appointment> appointments;
+}
