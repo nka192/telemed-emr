9cff2766228acc7126d62469b79d6730d5a45afe
Nayoung Kim <nka50@sfu.ca>
2025-11-15 23:53:29 -0800

    feat: enable asynchronous processing with @EnableAsync in main application

--- src/main/java/com/nayoung/telemed/TelemedEmrApplication.java
+++ src/main/java/com/nayoung/telemed/TelemedEmrApplication.java
@@ -2,8 +2,10 @@ package com.nayoung.telemed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TelemedEmrApplication {
 
 	public static void main(String[] args) {
