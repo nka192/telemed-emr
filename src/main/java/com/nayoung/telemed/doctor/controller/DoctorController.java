3557821fda94118fa8e9a74e68b83d38e19b4985
Nayoung Kim <nka50@sfu.ca>
2025-11-19 00:57:13 -0800

    feat(doctor): implement DoctorController with profile endpoints, role-based access, and specialization filtering

---
+++ src/main/java/com/nayoung/telemed/doctor/controller/DoctorController.java
@@ -0,0 +1,52 @@
+package com.nayoung.telemed.doctor.controller;
+
+import com.nayoung.telemed.doctor.dto.DoctorDTO;
+import com.nayoung.telemed.doctor.service.DoctorService;
+import com.nayoung.telemed.enums.Specialization;
+import com.nayoung.telemed.res.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/doctors")
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    @GetMapping("/me")
+    @PreAuthorize("hasAuthority('DOCTOR')")
+    public ResponseEntity<Response<DoctorDTO>> getDoctorProfile() {
+        return ResponseEntity.ok(doctorService.getDoctorProfile());
+    }
+
+    @PutMapping("/me")
+    @PreAuthorize("hasAuthority('DOCTOR')")
+    public ResponseEntity<Response<?>> updateDoctorProfile(@RequestBody DoctorDTO doctorDTO) {
+        return ResponseEntity.ok(doctorService.updateDoctorProfile(doctorDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<List<DoctorDTO>>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
+
+    @GetMapping("specializations")
+    public ResponseEntity<Response<List<Specialization>>> getAllSpecializationEnums() {
+        return ResponseEntity.ok(doctorService.getAllSpecializationEnums());
+    }
+
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<Response<DoctorDTO>> getDoctorById(@PathVariable Long doctorId) {
+        return ResponseEntity.ok(doctorService.getDoctorById(doctorId));
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<Response<List<DoctorDTO>>> getDoctorsBySpecialization
+            (@RequestParam(required = true) Specialization specialization) {
+        return ResponseEntity.ok(doctorService.getDoctorsBySpecialization(specialization));
+    }
+}
