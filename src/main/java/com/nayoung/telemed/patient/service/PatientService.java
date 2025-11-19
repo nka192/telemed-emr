013bb664d58a8e3fbf419e54d8911c5aeff05e80
Nayoung Kim <nka50@sfu.ca>
2025-11-18 21:29:04 -0800

    fix(patient): correct return types for getAllBloodGroupEnums and getAllGenotypeEnums

--- src/main/java/com/nayoung/telemed/patient/service/PatientService.java
+++ src/main/java/com/nayoung/telemed/patient/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.nayoung.telemed.patient.service;
 
+import com.nayoung.telemed.enums.BloodGroup;
+import com.nayoung.telemed.enums.Genotype;
 import com.nayoung.telemed.patient.dto.PatientDTO;
 import com.nayoung.telemed.res.Response;
 
@@ -9,7 +11,7 @@ public interface PatientService {
     Response<PatientDTO> getPatientProfile();
     Response<?> updatePatientProfile(PatientDTO patientDTO);
     Response<PatientDTO> getPatientById(Long patientId);
-    Response<List<PatientDTO>> getAllBloodGroupEnums();
-    Response<List<PatientDTO>> getAllGenotypeEnums();
+    Response<List<BloodGroup>> getAllBloodGroupEnums();
+    Response<List<Genotype>> getAllGenotypeEnums();
     Response<List<PatientDTO>> getAllPatients();
 }
